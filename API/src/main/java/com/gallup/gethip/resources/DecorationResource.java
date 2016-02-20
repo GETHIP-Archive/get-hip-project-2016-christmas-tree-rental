878891c6da9fa749b8baa45e3dd8674c13a00c3a
unknown <avguest@ORFHUSKERR940A.noam.gallup.com>
2016-02-20 12:59:42 -0600

    In class stuff

--- API/src/main/java/com/gallup/gethip/resources/DecorationResource.java
+++ API/src/main/java/com/gallup/gethip/resources/DecorationResource.java
@@ -1,22 +1,33 @@
 package com.gallup.gethip.resources;
+import java.sql.SQLException;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+
+import com.gallup.gethip.DataSourceManager;
+import com.gallup.gethip.model.Decoration;
 import com.gallup.gethip.resources.DecorationService;
+import com.j256.ormlite.dao.Dao;
 @Path("/api")
 public class DecorationResource {
 	//[Default] Outputs all decorations (example.com/api)
 	@GET
 	@Produces("application/json")
-	public static String rZero() {
+	public String rZero() {
 		return DecorationService.getAll();
 	}
 	//Outputs all decorations (example.com/api/all)
 	@Path("/all")
 	@GET
 	@Produces("application/json")
-	public static String rOne() {
+	public String rOne() {
 		return DecorationService.getAll();
 	}
 	//Outputs all decorations for the specified holiday (example.com/api/holiday/easter)
@@ -24,7 +35,7 @@ public class DecorationResource {
 	@GET
 	@Produces("application/json")
 	//Saves holiday name from URL to the String hday
-	public static String rTwo(@PathParam("hday") String hday) {
+	public String rTwo(@PathParam("hday") String hday) {
 		return DecorationService.getHoliday(hday);
 	}
 	//Outputs the product info of the product with the specified id (example.com/api/22)
@@ -32,7 +43,7 @@ public class DecorationResource {
 	@GET
 	@Produces("application/json")
 	//Saves product id from URL to the String id which will be converted to int in DecorationService method
-	public static String rThree(@PathParam("productid") String id) {
+	public String rThree(@PathParam("productid") String id) {
 		return DecorationService.getProductId(id);
 	}
 	//Outputs the product info of the product with the specified name (example.com/api/product/Plastic%20Tree) ~ URLs use '%20' instead of spaces
@@ -40,7 +51,16 @@ public class DecorationResource {
 	@GET
 	@Produces("application/json")
 	//Saves product name from URL to the String productname
-	public static String rFour(@PathParam("name") String productname) {
+	public String rFour(@PathParam("name") String productname) {
 		return DecorationService.getProductName(productname);
 	}
+	
+	@DELETE
+	@Produces("text/plain")
+	public String rFive(@PathParam("productid") String id){
+		return DecorationService.deleteDecoration(id);
+	}
+	
+	
+	
 }
--- API/src/main/java/com/gallup/gethip/resources/DecorationService.java
+++ API/src/main/java/com/gallup/gethip/resources/DecorationService.java
@@ -1,8 +1,21 @@
 package com.gallup.gethip.resources;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+
+import com.gallup.gethip.DataSourceManager;
+import com.gallup.gethip.model.Decoration;
 import com.gallup.gethip.resources.DatabaseResource;
+import com.j256.ormlite.dao.Dao;
 public class DecorationService {
 	//GET Methods
-	public static String getAll() {
+	public String getAll() {
 		//Declared 'output' outside of try/catch so if the database connection failed, the function could still return a string and not break the whole thing
 		String output;
 		output = "";
@@ -14,10 +27,12 @@ public class DecorationService {
 		}
 		return output;
 	}
-	public static String getHoliday(String h) {
+	public String getHoliday(String h) {
 		String output;
 		output = "";
+		
 		try {
+			List<Decoration> list = getDao().queryForEq("holiday", h);
 			DatabaseResource connect = new DatabaseResource();
 			output = connect.getData("SELECT * FROM decorations WHERE holiday = '" + h + "' ORDER BY name;");
 		} catch (Exception ex) {
@@ -25,7 +40,7 @@ public class DecorationService {
 		}
 		return output;
 	}
-	public static String getProductId(String i) {
+	public String getProductId(String i) {
 		String output;
 		output = "";
 		try {
@@ -37,7 +52,7 @@ public class DecorationService {
 		}
 		return output;
 	}
-	public static String getProductName(String n) {
+	public String getProductName(String n) {
 		String output;
 		output = "";
 		try {
@@ -47,5 +62,53 @@ public class DecorationService {
 			System.out.println("Error retrieving decorations by product name: " + ex);
 		}
 		return output;
-	}	
+	}
+	
+	
+	public String deleteDecoration(String id){
+    	try {
+			int num = getDao().deleteById(id);
+			if(num == 1){
+				return "Delete successful";
+			}else{
+				return "Unable to delete";
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return "Error in sql statement";
+		}
+    }
+	
+	public Decoration createDecoration(Decoration d){
+		try {
+			Decoration decPrime = getDao().createIfNotExists(d);
+			if(decPrime == null){
+				throw new NullPointerException();
+			}else{
+				return decPrime;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return  null;
+	}
+
+	public String updateDecoration(Decoration d){
+		try {
+			int num = getDao().update(d);
+			if(num == 1){
+				return "Update successful for user " + d.getID();
+			}else{
+				return "Could not update " + d.getID();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return "Error updating " + d.getID();
+		}
+	}
+
+	private Dao<Decoration, String> getDao(){
+		Dao<Decoration, String> dao = DataSourceManager.getInstance().getDao(Decoration.class);
+		return dao;
+	}
 }
