b77c75df9f71d6b9ebd73f557f30adb174dbd0e9
Thomas Gerot <tgerot179@millardps.org>
2016-02-21 12:38:48 -0600

    Added back JDBC/MySQL connection

--- API/src/main/java/com/gallup/gethip/Main.java
+++ API/src/main/java/com/gallup/gethip/Main.java
@@ -1,6 +1,4 @@
-//Package Declaration
 package com.gallup.gethip;
-//Import Jersey/Maven/HTTP stuff
 import com.gallup.gethip.model.Decoration;
 import com.gallup.gethip.model.Cart;
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
@@ -9,21 +7,37 @@ import com.sun.jersey.api.container.grizzly2.GrizzlyServerFactory;
 import com.sun.jersey.api.core.PackagesResourceConfig;
 import com.sun.jersey.api.core.ResourceConfig;
 import org.glassfish.grizzly.http.server.HttpServer;
-//Import finer Jersey / SQL functions
 import javax.ws.rs.core.UriBuilder;
 import java.io.IOException;
 import java.net.URI;
 import java.sql.SQLException;
 public class Main {
-    private static int getPort(int defaultPort) {
+	private static void createDatabaseConnection(){
+		String databaseUrl = "jdbc:mysql://jgetrost.com:3306/rental";
+		ConnectionSource connectionSource;
+		try {
+			connectionSource = new JdbcConnectionSource(databaseUrl);
+			((JdbcConnectionSource)connectionSource).setUsername("rental");
+			((JdbcConnectionSource)connectionSource).setPassword("get-hip");
+			DataSourceManager.setConnectionSource(connectionSource);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	private static void buildDaos(){
+		try {
+			DataSourceManager.addDao(Employee.class);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+    private static int getPort(int defaultPort) 
         String httpPort = System.getProperty("jersey.test.port");
         if (null != httpPort) {
             try {
-	    	int outport = Integer.parseInt(httpPort);
-		System.out.println("Success: Retrieved jersey HTTP port (" + outport + ")");
-                return outport;
+                return Integer.parseInt(httpPort);
             } catch (NumberFormatException e) {
-	    	System.err.println("Failed. Incorrect number format: " + e);
+            	System.out.println("Error getting port: " + e);
             }
         }
         return defaultPort;
@@ -34,12 +48,15 @@ public class Main {
     public static final URI BASE_URI = getBaseURI();
     protected static HttpServer startServer() throws IOException {
         ResourceConfig resourceConfig = new PackagesResourceConfig("com.gallup.gethip.resources");
-        System.out.println("Starting HTTP Server");
+        System.out.println("Starting grizzly2...");
         return GrizzlyServerFactory.createHttpServer(BASE_URI, resourceConfig);
     }
     public static void main(String[] args) throws IOException {
         HttpServer httpServer = startServer();
-	System.in.read();
-	httpServer.stop();
+        System.out.println(String.format("Jersey app started with WADL available at " + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
+        createDatabaseConnection();
+        buildDaos();
+        System.in.read();
+        httpServer.stop();
     }    
 }
